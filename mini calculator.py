904fecc99bf57b26fcb04289abb22ffed31aee87
Tarun-learner <tarunjangra675@gmail.com>
2025-05-02 01:57:37 +0530

    Add files via upload

---
+++ mini calculator.py
@@ -0,0 +1,22 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2639
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 num1 = int(input("enter the 1st number")) # take input as\
+user\
+sign = input("enter any sign you want to calculate ")\
+num2 = int(input("enter the second number "))\
+#check which sign user entered\
+if sign ==\'91+\'92:\
+print("the result is: "num1+num2)\
+elif sign == \'91-\'91 \
+print("the result is : "num1-num2)\
+elif sign =="*' :\
+print("the result is : "num1*num2)\
+elif sign =="/'\
+print("the result is : "num1 / num2 )\
+else:\
+print("invalid number")}
